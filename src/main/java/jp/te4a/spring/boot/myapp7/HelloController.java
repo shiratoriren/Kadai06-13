e9206a4bdff9c8319f14ed0e370bec202c4e5782
190457 <190457@e0601-15.JC-21.local>
2022-05-20 12:20:42 +0900

    Change:Thymeleafを使うようにした #2

--- src/main/java/jp/te4a/spring/boot/myapp7/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp7/HelloController.java
@@ -12,26 +12,19 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloController {
     @Autowired
     BookService bookService;
-    @RequestMapping("/")
+    @RequestMapping("books/list")
     public String index(Model model) {
         model.addAttribute("msg", "this is setting message");
-        return "index";
+        return "books/list";
     }
     
-    @RequestMapping(value="/post", method=RequestMethod.POST)
-     public ModelAndView postForm(@RequestParam("id") String id, 
+    @RequestMapping(value="books/list", method=RequestMethod.POST)
+    public ModelAndView postForm(@RequestParam("id") String id, 
     @RequestParam("title") String title,@RequestParam("writter") String writter, 
     @RequestParam("publisher") String publisher,@RequestParam("price") String price) {
-         ModelAndView mv = new ModelAndView("index");
-         bookService.save(new BookBean(Integer.valueOf(id),title,writter,publisher,Integer.valueOf(price)));
-        StringBuffer buff = new StringBuffer();
-        buff.append("<HR>");
-        for(BookBean bean:bookService.findAll()) {
-            buff.append("ID:" + bean.getId() + "<BR>" + "タイトル:" + bean.getTitle() + 
-           "<BR>"+ "著者:" + bean.getWritter() + "<BR>" + "出版社:" + bean.getPublisher() + 
-           "<BR>"+ "価格:" + bean.getPrice() + "<BR><HR>");
-        }
-        mv.addObject("msg", buff.toString());
+         ModelAndView mv = new ModelAndView("books/list");
+         bookService.save(new BookBean(Integer.valueOf(id),title,writter,publisher,Integer.valueOf(price)));    
+        mv.addObject("books", bookService.findAll());
         return mv;
     }
 }
