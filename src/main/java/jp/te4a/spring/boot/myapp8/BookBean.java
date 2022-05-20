2c7bb3bd16cbca1c013f1f79fdf900c96b7d265f
190457 <190457@e0601-15.JC-21.local>
2022-05-20 14:32:47 +0900

    Change:RESTを使うようにした #3

--- src/main/java/jp/te4a/spring/boot/myapp8/BookBean.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/BookBean.java
@@ -1,12 +1,14 @@
 package jp.te4a.spring.boot.myapp8;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookBean  {
-	Integer id ;
+	Integer id;
 	String title;
 	String writter;
 	String publisher;
