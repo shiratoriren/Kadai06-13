e7c315ae7f4fc1a24fcade0cb5516dc456edeafa
190457 <190457@e0601-15.JC-21.local>
2022-05-25 10:43:33 +0900

    Add:入力項目に以下のバリデーションを追加 タイトル:入力必須,３文字以上 著者:３文字以上,２０文字以下 価格:０以上の数値

--- src/main/java/jp/te4a/spring/boot/myapp11/BookForm.java
+++ src/main/java/jp/te4a/spring/boot/myapp11/BookForm.java
@@ -1,5 +1,8 @@
 package jp.te4a.spring.boot.myapp11;
 //import lombok.AllArgsConstructor;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,8 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BookForm {
   private Integer id ;
+  @NotNull
+  @Size(min=3)
   private String title;
+  @Size(min=3,max=20)
   private String writter;
   private String publisher;
+  @Min(0)
   private Integer price;
 }
